0852f86f9bf920d006bad291957d829120f21a9c
Lealen <git@lealen.pl>
2016-12-26 21:05:15 +0100

    Package is now able to handle correctly all data functions

--- data.go
+++ data.go
@@ -11,17 +11,11 @@ import (
 
 var data = make(map[string]interface{})
 
-//Data is used to group all data-related functions in this package
-var Data *datastr
-
-type datastr struct {
-}
-
-func (*datastr) Get(name string) interface{} {
+func Get(name string) interface{} {
 	return data[name]
 }
 
-func (*datastr) Set(name string, value interface{}) {
+func Set(name string, value interface{}) {
 	data[name] = value
 
 	//UPDATE ON DOCUMENT:
@@ -32,7 +26,7 @@ func (*datastr) Set(name string, value interface{}) {
 	Tick()
 }
 
-func (*datastr) GetIndex(name string, index interface{}) interface{} {
+func GetIndex(name string, index interface{}) interface{} {
 	if _, ok := data[name]; !ok {
 		return nil
 	}
@@ -53,7 +47,7 @@ func (*datastr) GetIndex(name string, index interface{}) interface{} {
 	}
 }
 
-func (*datastr) SetIndex(name string, index, value interface{}) (interface{}, error) {
+func SetIndex(name string, index, value interface{}) (interface{}, error) {
 	if _, ok := data[name]; !ok {
 		return nil, fmt.Errorf("You need to define string/array/slice/map first")
 	}
@@ -68,11 +62,13 @@ func (*datastr) SetIndex(name string, index, value interface{}) (interface{}, er
 
 	if valueArray.Kind() == reflect.Map {
 		valueArray.SetMapIndex(valueIndex, valueA)
+		data[name] = valueArray.Interface()
 	} else {
 		if valueArray.Len() <= int(valueIndex.Int()) {
 			return nil, fmt.Errorf("Index out of range.")
 		}
 		valueArray.Index(int(valueIndex.Int())).Set(valueA)
+		data[name] = valueArray.Interface()
 	}
 
 	//UPDATE ON DOCUMENT:
@@ -100,7 +96,6 @@ func (*datastr) SetIndex(name string, index, value interface{}) (interface{}, er
 		wheretoadd := "beforeend"
 
 		if ei != nil && ei != js.Undefined && jsbuiltin.TypeOf(ei) != "undefined" {
-			println(ei)
 			//get a previous element as long it isn't a goob-index, so we will not need to create more childs like div or something..
 			ei = ei.Get("previousSibling")
 			for ei != nil && ei != js.Undefined && jsbuiltin.TypeOf(ei) != "undefined" && !(jsbuiltin.TypeOf(ei.Get("hasAttribute")) == "function" && ei.Call("hasAttribute", goobIndex).Bool()) {
@@ -114,13 +109,13 @@ func (*datastr) SetIndex(name string, index, value interface{}) (interface{}, er
 			if valueArray.Kind() == reflect.Map {
 				buf.WriteString("<div style=\"display:none;\" " + goobIndex + "=\"" + fmt.Sprintf("%v", valueIndex.Interface()) + "\"></div>")
 			} else {
-				buf.WriteString("<div style=\"display:none;\" goob-index></div>")
+				buf.WriteString("<div style=\"display:none;\" " + goobIndex + "></div>")
 			}
 		}
 
 		if valueArray.Kind() == reflect.Map {
 			if everyindex.Length() == 0 {
-				println(buf.String(), wheretoadd)
+				// println(buf.String(), wheretoadd)
 				every.Index(i).Call("insertAdjacentHTML", wheretoadd, buf.String())
 			} else {
 				for j := 0; j < everyindex.Length(); j++ {
@@ -142,7 +137,7 @@ func (*datastr) SetIndex(name string, index, value interface{}) (interface{}, er
 }
 
 //Push is pushing value at the end of slice or string
-func (*datastr) Push(name string, value interface{}) (interface{}, error) {
+func Push(name string, value interface{}) (interface{}, error) {
 	if _, ok := data[name]; !ok {
 		return nil, fmt.Errorf("You need to define slice first")
 	}
@@ -164,29 +159,47 @@ func (*datastr) Push(name string, value interface{}) (interface{}, error) {
 		return nil, fmt.Errorf("Type isnt slice or string.")
 	}
 
+	//UPDATE ON DOCUMENT:
+	every := js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"]["+goobForeach+"]")
+	for i := 0; i < every.Length(); i++ {
+		var buf bytes.Buffer
+		err := tmpl.ExecuteTemplate(&buf, every.Index(i).Call("getAttribute", goobBind).String(), value)
+		if err != nil {
+			println(err)
+		}
+
+		buf.WriteString("<div style=\"display:none;\" " + goobIndex + "></div>")
+		every.Index(i).Call("insertAdjacentHTML", "beforeend", buf.String())
+	}
+	every = js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"]:not(["+goobForeach+"]), ["+goobWatch+"=\""+name+"\"]:not(["+goobForeach+"])")
+	for i := 0; i < every.Length(); i++ {
+		every.Index(i).Call("removeAttribute", goobProcessed)
+	}
+	Tick()
+
 	return data[name], nil
 }
 
-//Join appends a slice to a slice value. The slices must have the same element type.
-func (*datastr) Join(name string, value interface{}) (interface{}, error) {
-	if _, ok := data[name]; !ok {
-		return nil, fmt.Errorf("You need to define slice first")
-	}
+// TODO
+// //Join appends a slice to a slice value. The slices must have the same element type.
+// func Join(name string, value interface{}) (interface{}, error) {
+// 	if _, ok := data[name]; !ok {
+// 		return nil, fmt.Errorf("You need to define slice first")
+// 	}
+//
+// 	valueArray := reflect.ValueOf(data[name])
+// 	valueA := reflect.ValueOf(value)
+//
+// 	if valueArray.Kind() == reflect.Slice && valueA.Kind() == reflect.Slice {
+// 		data[name] = reflect.AppendSlice(valueArray, valueA).Interface()
+// 	} else {
+// 		return nil, fmt.Errorf("Both types needs to be a slice.")
+// 	}
+//
+// 	return data[name], nil
+// }
 
-	valueArray := reflect.ValueOf(data[name])
-	valueA := reflect.ValueOf(value)
-
-	if valueArray.Kind() == reflect.Slice && valueA.Kind() == reflect.Slice {
-		data[name] = reflect.AppendSlice(valueArray, valueA).Interface()
-	} else {
-		return nil, fmt.Errorf("Both types needs to be a slice.")
-	}
-
-	return data[name], nil
-}
-
-//TODO
-func (*datastr) InsertAt(name string, value interface{}) (interface{}, error) {
+func InsertAt(name string, index int, value interface{}) (interface{}, error) {
 	if _, ok := data[name]; !ok {
 		return nil, fmt.Errorf("You need to define slice first")
 	}
@@ -196,17 +209,116 @@ func (*datastr) InsertAt(name string, value interface{}) (interface{}, error) {
 
 	switch valueArray.Kind() {
 	case reflect.Slice:
-		data[name] = reflect.Append(valueArray, valueA).Interface()
+		valueArray = reflect.Append(valueArray, valueA)
+		reflect.Copy(valueArray.Slice(index+1, valueArray.Len()), valueArray.Slice(index, valueArray.Len()-1))
+		valueArray.Index(index).Set(valueA)
+		data[name] = valueArray.Interface()
 	case reflect.String:
 		switch valueA.Kind() {
 		case reflect.String:
-			data[name] = valueArray.String() + valueA.String()
+			data[name] = valueArray.Slice(0, index).String() + valueA.String() + valueArray.Slice(index, valueArray.Len()).String()
 		default:
 			return nil, fmt.Errorf("Value needs to be a type of string.")
 		}
 	default:
 		return nil, fmt.Errorf("Type isnt slice or string.")
 	}
+
+	//UPDATE ON DOCUMENT:
+	every := js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"]["+goobForeach+"]")
+	for i := 0; i < every.Length(); i++ {
+		var ei *js.Object
+		wheretoadd := "afterend"
+
+		if index == 0 {
+			ei = every.Index(i)
+			wheretoadd = "afterbegin"
+		} else {
+			ei = every.Index(i).Call("querySelectorAll", "["+goobIndex+"]").Index(index - 1)
+		}
+
+		var buf bytes.Buffer
+		err := tmpl.ExecuteTemplate(&buf, every.Index(i).Call("getAttribute", goobBind).String(), value)
+		if err != nil {
+			println(err)
+		}
+
+		buf.WriteString("<div style=\"display:none;\" " + goobIndex + "></div>")
+		ei.Call("insertAdjacentHTML", wheretoadd, buf.String())
+	}
+	every = js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"]:not(["+goobForeach+"]), ["+goobWatch+"=\""+name+"\"]:not(["+goobForeach+"])")
+	for i := 0; i < every.Length(); i++ {
+		every.Index(i).Call("removeAttribute", goobProcessed)
+	}
+	Tick()
+
+	return data[name], nil
+}
+
+//TODO: map
+func RemoveAt(name string, index int) (interface{}, error) {
+	if _, ok := data[name]; !ok {
+		return nil, fmt.Errorf("You need to define slice first")
+	}
+
+	valueArray := reflect.ValueOf(data[name])
+
+	switch valueArray.Kind() {
+	case reflect.Slice:
+		valueArray = reflect.AppendSlice(valueArray.Slice(0, index), valueArray.Slice(index+1, valueArray.Len()))
+		data[name] = valueArray.Interface()
+	default:
+		return nil, fmt.Errorf("Type isnt slice.")
+	}
+
+	//UPDATE ON DOCUMENT:
+	every := js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"]["+goobForeach+"]")
+	for i := 0; i < every.Length(); i++ {
+		ei := every.Index(i).Call("querySelectorAll", "["+goobIndex+"]").Index(index)
+
+		if ei != nil && ei != js.Undefined && jsbuiltin.TypeOf(ei) != "undefined" {
+			//get a previous element as long it isn't a goob-index, so we will not need to create more childs like div or something..
+			// ei = ei.Get("previousSibling")
+			einew := ei.Get("previousSibling")
+			ei.Call("remove") //TODO: check browser compatybility
+			ei = einew
+			for ei != nil && ei != js.Undefined && jsbuiltin.TypeOf(ei) != "undefined" && !(jsbuiltin.TypeOf(ei.Get("hasAttribute")) == "function" && ei.Call("hasAttribute", goobIndex).Bool()) {
+				einew = ei.Get("previousSibling")
+				ei.Call("remove") //TODO: check browser compatybility
+				ei = einew
+			}
+		}
+	}
+	every = js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"]:not(["+goobForeach+"]), ["+goobWatch+"=\""+name+"\"]:not(["+goobForeach+"])")
+	for i := 0; i < every.Length(); i++ {
+		every.Index(i).Call("removeAttribute", goobProcessed)
+	}
+	Tick()
+
+	return data[name], nil
+}
+
+//TODO: map
+func Empty(name string) (interface{}, error) {
+	if _, ok := data[name]; !ok {
+		return nil, fmt.Errorf("You need to define slice first")
+	}
+
+	valueArray := reflect.ValueOf(data[name])
+
+	switch valueArray.Kind() {
+	case reflect.Slice:
+		data[name] = valueArray.Slice(0, 0).Interface()
+	default:
+		return nil, fmt.Errorf("Type isnt slice.")
+	}
+
+	//UPDATE ON DOCUMENT:
+	every := js.Global.Get("document").Call("querySelectorAll", "["+goobData+"=\""+name+"\"], ["+goobWatch+"=\""+name+"\"]")
+	for i := 0; i < every.Length(); i++ {
+		every.Index(i).Call("removeAttribute", goobProcessed)
+	}
+	Tick()
 
 	return data[name], nil
 }
